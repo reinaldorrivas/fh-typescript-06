b78b6352e176e3635e6a82ea5a75eef6c4d0f682
René Rivas <reinaldorrivas@gmail.com>
2026-02-15 12:41:26 -0400

    fh-ts-guide: Singleton TS intro.

--- src/Classes/privateConstructor.ts
+++ src/Classes/privateConstructor.ts
@@ -1,3 +1,41 @@
 export const privateConstructor = (): void => {
+  class SingletonTs {
+    private static instance: SingletonTs;
+
+    public static setInstance(value: string): SingletonTs {
+      if (!SingletonTs.instance) {
+        SingletonTs.instance = new SingletonTs(value);
+      }
+
+      return SingletonTs.instance;
+    }
+
+    private constructor(private _name: string) {}
+
+    get name(): string {
+      return this._name;
+    }
+
+    set name(value: string) {
+      this._name = value;
+    }
+  }
+
+  const wolverine = SingletonTs.setInstance("Wolverine");
+  const magneto = SingletonTs.setInstance("Magneto");
+  const ironman = SingletonTs.setInstance("Ironman");
+
+  /*
+   * Esto no es permitido porque el constructor es privado:
+   * const lola = new SingletonTs("Lola");
+   */
+
+  console.table([wolverine, magneto, ironman]);
+
+  // * Esto modifica todas las variables que usen el Singleton porque todas estas apuntan al mismo espacio de memoria.
+  wolverine.name = "Lalo";
+
+  console.table([wolverine, magneto, ironman]);
+
   console.log("--- END PRIVATE CONSTRUCTOR FILE ---");
 };
